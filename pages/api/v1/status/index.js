434cb3a824f057bf53a7a76e1336dab7067f0c9c
Edilson Novaes <59837874+edilsonnovaes@users.noreply.github.com>
2025-02-25 19:00:47 +0000

    Add `database.js` and `.env` files

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,9 @@
-function status(request, response) {
+import database from "../../../../infra/database.js";
+
+async function status(request, response) {
+  const result = await database.query('SELECT 1 + 1 as sum;');
+  console.log(result.rows);
+  
   return response.json({ chave: "Retornando a primeira function da api." });
 }
 export default status;
