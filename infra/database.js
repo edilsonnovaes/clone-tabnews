b32145e759ebeae7e3d532c2713fc59c66980f92
Edilson Novaes <59837874+edilsonnovaes@users.noreply.github.com>
2025-03-18 21:24:46 +0000

    handles database ssl connection

--- infra/database.js
+++ infra/database.js
@@ -7,13 +7,7 @@ async function query(queryObject) {
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
-  });
-  console.log("credenciasis", {
-    host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
-    user: process.env.POSTGRES_USER,
-    database: process.env.POSTGRES_DB,
-    password: process.env.POSTGRES_PASSWORD,
+    ssl: getSSLValues(),
   });
 
   try {
@@ -22,7 +16,7 @@ async function query(queryObject) {
     return result;
   } catch (err) {
     console.error(err);
-    throw error;
+    throw err;
   } finally {
     await client.end();
   }
@@ -31,3 +25,13 @@ async function query(queryObject) {
 export default {
   query: query,
 };
+
+function getSSLValues() {
+  if (process.env.POSTGRES_CA) {
+    return {
+      ca: process.env.POSTGRES_CA,
+    };
+  }
+  
+  return process.env.NODE_ENV === "development" ? false : true;
+}
