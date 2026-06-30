ae5eea3961415e56f3c3fffeee68d9b2b6f04323
Edilson Novaes <59837874+edilsonnovaes@users.noreply.github.com>
2026-06-30 22:35:53 +0000

    feat: add `features` column `users` table

--- tests/integration/api/v1/users/post.test.js
+++ tests/integration/api/v1/users/post.test.js
@@ -32,6 +32,7 @@ describe("POST /api/v1/users", () => {
         id: responseBody.id,
         username: "edilsonnovaes",
         email: "edilson.junior96@hotmail.com",
+        features: [],
         password: responseBody.password,
         created_at: responseBody.created_at,
         updated_at: responseBody.updated_at,
