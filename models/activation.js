b19067ef27340cf2ff065e94aab0266dcdd843f2
Edilson Novaes <59837874+edilsonnovaes@users.noreply.github.com>
2026-07-14 17:32:07 +0000

    feat: require `update:user` to access `/api/v1/users/[username]`

--- models/activation.js
+++ models/activation.js
@@ -66,6 +66,7 @@ async function activateUserByUserId(userId) {
   const activatedUser = await user.setFeatures(userId, [
     "create:session",
     "read:session",
+    "update:user",
   ]);
   return activatedUser;
 }
