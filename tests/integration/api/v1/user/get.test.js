5735d700df255d013b20eb53046f0f00acea80b1
Edilson Novaes <59837874+edilsonnovaes@users.noreply.github.com>
2026-07-15 14:17:57 +0000

    feat: apply `authorization.filterOutput()` to all endpoints

--- tests/integration/api/v1/user/get.test.js
+++ tests/integration/api/v1/user/get.test.js
@@ -39,7 +39,6 @@ describe("GET /api/v1/user", () => {
         username: "UserWithValidSession",
         email: createdUser.email,
         features: ["create:session", "read:session", "update:user"],
-        password: createdUser.password,
         created_at: createdUser.created_at.toISOString(),
         updated_at: actvatedUser.updated_at.toISOString(),
       });
